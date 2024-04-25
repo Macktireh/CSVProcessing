a5f1d6eb715cc33616cd258151d048543eaad89e
Macktireh Abdi Soubaneh <abdimack97@gmail.com>
2024-04-25 17:46:17 +0200

    typing added to read_csv_file_names function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+from typing import List
 
 from sfs_processing import SFSHistoDataProcessor
 
 INPUT_DIR = 'inputs'
 
 
-def read_csv_file_names():
+def read_csv_file_names() -> List[str]:
     """read CSV file names from the 'inputs' folder"""
     return [f for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f)) and f.endswith('.csv')]
 
