10b2063a388543d491952bd13aa8f0b4643a58c7
Macktireh Abdi Soubaneh <abdimack97@gmail.com>
2024-10-16 13:37:42 +0200

    Code refactoring and add requrements.txt file

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 from typing import List
 
+import columns
 from sfs_processing import SFSHistoDataProcessor
 
-INPUT_DIR = 'inputs'
+INPUT_DIR = "inputs"
 
 
 def read_csv_file_names() -> List[str]:
     """read CSV file names from the 'inputs' folder"""
-    return [f for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f)) and f.endswith('.csv')]
+    return [
+        f for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f)) and f.endswith(".csv")
+    ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_names = read_csv_file_names()
     processor = SFSHistoDataProcessor(file_names, INPUT_DIR)
+    processor.setup_columns(
+        drop_columns_list=columns.drop_columns,
+        renames_dict=columns.renames_dict,
+        new_columns_list=columns.new_columns,
+        reordered_columns_list=columns.reordered_columns,
+    )
     processor.start()
--- sfs_processing.py
+++ sfs_processing.py
@@ -10,62 +10,95 @@ class SFSHistoDataProcessor:
 
     df: pd.DataFrame
 
-    def __init__(self, file_names: Union[str, List[str]], input_dir: str = "inputs"):
+    def __init__(self, file_names: Union[str, List[str]], input_dir: str = "inputs") -> None:
         self.file_names = file_names
         self.input_dir = input_dir
 
-    def read_csv(self, file_name: str):
+    def setup_columns(
+        self,
+        drop_columns_list: list[str],
+        renames_dict: dict[str, str],
+        new_columns_list: list[str],
+        reordered_columns_list: list[str],
+    ) -> None:
+        self.drop_columns_list = drop_columns_list
+        self.renames_dict = renames_dict
+        self.new_columns_list = new_columns_list
+        self.reordered_columns_list = reordered_columns_list
+
+    def read_csv(self, file_name: str) -> None:
         """Reads the CSV file into a DataFrame."""
         path_input_file = f"{self.input_dir}/{file_name}"
         self.df = pd.read_csv(path_input_file)
 
-    def rename_columns(self):
-        """Renames the columns of the DataFrame."""
-        renames_dict = {
-            "[v__SHOP]":"[NberShopActivity]",
-            "[v__SHOP2]":"[PerShopActivity]",
-            "[v__Total_Sites_Active]":"[TotalSitesActive]",
-            "[v__Food_Services2]":"[NberFoodServicesActivity]",
-            "[v__Food_Services]":"[PerFoodServicesActivity]",
-            "[v__Car_Services2]":"[NberCarServicesActivity]",
-            "[v__Car_Services]":"[PerCarServicesActivity]",
-            "[v__CarWash2]":"[NberCarWashActivity]",
-            "[v__CarWash]":"[PerCarWashActivity]",
-        }
-        self.df.rename(columns = renames_dict, inplace = True)
+    def delete_rows(self) -> None:
+        self.df.drop(self.df[self.df["[IsGrandTotalRowTotal]"] == True].index, inplace=True)  # noqa: E712
 
-    def drop_column(self):
+    def drop_columns(self) -> None:
         """Drops a specific column from the DataFrame."""
-        self.df.drop(columns=['Geography[Id SubZone]'], inplace=True)
+        self.df.drop(columns=self.drop_columns_list, inplace=True)
 
-    def reorder_columns(self):
-        """Reorders the columns of the DataFrame."""
-        reordered_columns = [
-            "[IsGrandTotalRowTotal]",
-            "[TotalSitesActive]",
-            "[NberShopActivity]",
-            "[PerShopActivity]",
-            "[NberFoodServicesActivity]",
-            "[PerFoodServicesActivity]",
-            "[NberCarServicesActivity]",
-            "[PerCarServicesActivity]",
-            "[NberCarWashActivity]",
-            "[PerCarWashActivity]",
-            "Geography[Zone]",
-            "Geography[Sub Zone]",
-            "Geography[Affiliates]"
-        ]
-        self.df = self.df.reindex(columns=reordered_columns)
+    def rename_columns(self) -> None:
+        """Renames the columns of the DataFrame."""
+        # renames_dict = {
+        #     "Geography[Zone]": "[Zone]",
+        #     "Geography[Sub Zone]": "[SubZone]",
+        #     "Geography[Affiliates]": "[Affiliates]",
+        # }
+        self.df.rename(columns=self.renames_dict, inplace=True)
 
-    def insert_columns(self):
+    def insert_columns(self) -> None:
         """Inserts new columns into the DataFrame."""
-        self.df.insert(loc=1, column="[TotalSites]", value=None, allow_duplicates=False)
-        self.df.insert(loc=11, column="[NberHotBeverageActivity]", value=None, allow_duplicates=False)
-        self.df.insert(loc=12, column="[PerHotBeverageActivity]", value=None, allow_duplicates=False)
-        self.df.insert(loc=13, column="[NberServicesActivity]", value=None, allow_duplicates=False)
-        self.df.insert(loc=14, column="[PerServicesActivity]", value=None, allow_duplicates=False)
+        # new_columns = [
+        #     "[NberNoShopActivity]",
+        #     "[PerNoShopActivity]",
+        #     "[NberNotDeclaredShopActivity]",
+        #     "[PerNotDeclaredShopActivity]",
+        #     "[NberNoFoodServicesActivity]",
+        #     "[PerNoFoodServicesActivity]",
+        #     "[NberNotDeclaredFoodServicesActivity]",
+        #     "[PerNotDeclaredFoodServicesActivity]",
+        #     "[NberNoHotBeverageActivity]",
+        #     "[PerNoHotBeverageActivity]",
+        #     "[NberNotDeclaredHotBeverageActivity]",
+        #     "[PerNotDeclaredHotBeverageActivity]",
+        #     "[NberNoCarServicesActivity]",
+        #     "[PerNoCarServicesActivity]",
+        #     "[NberNotDeclaredCarServicesActivity]",
+        #     "[PerNotDeclaredCarServicesActivity]",
+        #     "[NberNoCarWashActivity]",
+        #     "[PerNoCarWashActivity]",
+        #     "[NberNotDeclaredCarwashActivity]",
+        #     "[PerNotDeclaredCarwashActivity]",
+        #     "[NberNoServicesActivity]",
+        #     "[PerNoServicesActivity]",
+        #     "[NberNotDeclaredServicesActivity]",
+        #     "[PerNotDeclaredServicesActivity]",
+        # ]
+        
+        for column in self.new_columns_list:
+            self.df.insert(loc=1, column=column, value=None, allow_duplicates=False)
 
-    def export_to_csv(self, file_name: str):
+    def reorder_columns(self) -> None:
+        """Reorders the columns of the DataFrame."""
+        # reordered_columns = [
+        #     "[IsGrandTotalRowTotal]",
+        #     "[TotalSitesActive]",
+        #     "[NberShopActivity]",
+        #     "[PerShopActivity]",
+        #     "[NberFoodServicesActivity]",
+        #     "[PerFoodServicesActivity]",
+        #     "[NberCarServicesActivity]",
+        #     "[PerCarServicesActivity]",
+        #     "[NberCarWashActivity]",
+        #     "[PerCarWashActivity]",
+        #     "Geography[Zone]",
+        #     "Geography[Sub Zone]",
+        #     "Geography[Affiliates]",
+        # ]
+        self.df = self.df.reindex(columns=self.reordered_columns_list)
+
+    def export_to_csv(self, file_name: str) -> None:
         """Exports the DataFrame to a new CSV file."""
         output_dir = "outputs"
         path_output_file = f"{output_dir}/{file_name}"
@@ -73,7 +106,7 @@ class SFSHistoDataProcessor:
             os.makedirs(output_dir)
         self.df.to_csv(path_output_file, index=False)
 
-    def process(self, file_name: str):
+    def process(self, file_name: str) -> None:
         """
         Executes all the processing methods in order.
 
@@ -86,12 +119,13 @@ class SFSHistoDataProcessor:
         - export_to_csv
         """
         self.read_csv(file_name)
+        self.delete_rows()
+        self.drop_columns()
         self.rename_columns()
-        self.drop_column()
-        self.reorder_columns()
         self.insert_columns()
+        self.reorder_columns()
         self.export_to_csv(file_name)
-    
+
     def start(self):
         """Starts the processing of the CSV files."""
         if isinstance(self.file_names, list):
